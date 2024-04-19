2a8b9ab2c25ef2b22367976928864899629e6751
nedmad <matheusmned@gmail.com>
2024-04-19 01:08:05 -0300

    Updates

---
+++ imagens.js
@@ -0,0 +1,14 @@
+
+
+export {default as fundo} from "./fundo.jpg";
+export {default as me} from "./me.jpg";
+export {default as curriculoImage} from "./curriculo.png"
+export {default as githubImage} from "./github-mark.png";
+export {default as linkedinImage} from "./linkedin.png";
+export {default as htmlImage} from "./html.png";
+export {default as cssImage} from "./css.png";
+export {default as jsImage} from "./js.png";
+export {default as reactImage} from "./react.png";
+export {default as sassImage} from "./sass.png";
+export {default as gitImage} from "./git.png";
+export {default as pdf} from "./curriculo.pdf";
