504efd87c3c5acba2f5339e286ccb39f0233282b
nedmad <matheusmned@gmail.com>
2024-04-19 02:40:12 -0300

    mudei

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    entries: {
-      base1: ["/matheus-ned-portifolio/"],
-      base2: ["/matheus-ned-portifolio/tecnologia"],
-      base3: ["/matheus-ned-portifolio/projetos"],
-    },
-  },
-  plugins: [react()],
-});
+  base:"/matheus-ned-portifolio",
+  plugins: [react()]
+
+})
