bd7dbf14ec4222ebb8489db2967856ad3c50c598
nedmad <matheusmned@gmail.com>
2024-04-19 11:06:36 -0300

    outras rotas

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,32 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),reactRefresh()],
-  base: "/matheus-ned-portifolio/",
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+  base:"/matheus-ned-portifolio",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@dir1': '/matheus-ned-portifolio',
+      '@dir2': '/matheus-ned-portifolio/tecnologia',
+      // Adicione mais aliases conforme necessário para outros diretórios
     },
   },
-  build: {
-    outDir: "docs",
-    assetsDir: ".",
+  // Configuração do servidor para lidar com rotas de diretórios
+  server: {
+    proxy: {
+      '/diretorio1': {
+        target: 'http://localhost:3000', // Endereço do servidor para o diretório 1
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/diretorio1/, '/matheus-ned-portifolio'),
+      },
+      '/diretorio2': {
+        target: 'http://localhost:3000', // Endereço do servidor para o diretório 2
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/diretorio2/, '/matheus-ned-portifolio/tecnologia'),
+      },
+      // Adicione mais configurações de proxy conforme necessário para outros diretórios
+    },
   },
-});
+
+})
