42eae565a954b13fc4f6ac3d1e302e3943bef0a5
nedmad <matheusmned@gmail.com>
2024-04-20 09:18:12 -0300

    agora vai

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '//matheus-ned-portifolio/',
+    base: '/matheus-ned-portifolio/',
     plugins: [react()]
+  
 
 })
