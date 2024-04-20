28804766ff22ce38b755c1deb7a4a96fc4e031b8
nedmad <matheusmned@gmail.com>
2024-04-20 02:22:29 -0300

    arrumando essa bagaca

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:"/matheus-ned-portifolio",
-  plugins: [react()],
-  
+export default defineConfig(({ command })=>{
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/matheus-ned-portifolio/'
+  }
+
+  return config
 
 })
