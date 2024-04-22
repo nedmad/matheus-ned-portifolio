2a7a911ba9ca3ed00bff966a820e87226db7fac9
nedmad <matheusmned@gmail.com>
2024-04-22 10:41:39 -0300

    portifolio finalizado

--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -1,21 +1,29 @@
-import { infolibre } from "../../public/imagens"
+import { infolibre } from "../../public/imagens";
 
 export default function Projetos() {
-    return (
-        <>
-            <div className="project">
-                <div className="cart" data-aos="fade-down">
-                    <div className="imageProject" style={{backgroundImage:`url(${infolibre})`}}></div>
-                    <div className="conteudoProject">
-                        <h3>Infolibre markert</h3>
-                       <p>
-                        Um projeto feito por min, com o objeivo de vender produtos e infoprodutos afiliados.
-                        Site construído com react, sass e bootstrap.
-                       </p>
-                        <button className="btn bt-primary">Visitar site</button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="project">
+        <div className="cart" data-aos="fade-down">
+          <div
+            className="imageProject"
+            style={{ backgroundImage: `url(${infolibre})` }}
+          ></div>
+          <div className="conteudoProject">
+            <h3>Infolibre markert</h3>
+            <p>
+              Um projeto feito por min, com o objeivo de vender produtos e
+              infoprodutos afiliados. Site construído com react, sass e
+              bootstrap.
+            </p>
+            <button className="btn bt-primary">
+              <a href="https://nedmad.github.io/infolibre/" target="_blank">
+                Visitar site
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
