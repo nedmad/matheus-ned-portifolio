8aa98e4de8dc30d81110e87c04af27dcb040d4a5
nedmad <matheusmned@gmail.com>
2024-04-22 00:55:48 -0300

    portifolio semifinalizado

--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -1,18 +1,18 @@
-
+import { infolibre } from "../../public/imagens"
 
 export default function Projetos() {
     return (
         <>
             <div className="project">
                 <div className="cart" data-aos="fade-down">
-                    <div className="imageProject"></div>
+                    <div className="imageProject" style={{backgroundImage:`url(${infolibre})`}}></div>
                     <div className="conteudoProject">
                         <h3>Infolibre markert</h3>
                        <p>
                         Um projeto feito por min, com o objeivo de vender produtos e infoprodutos afiliados.
                         Site construído com react, sass e bootstrap.
                        </p>
-
+                        <button className="btn bt-primary">Visitar site</button>
                     </div>
                 </div>
             </div>
